22f18b4a9140b4280cbf921870b2b9e64ecc3321
Mecheter Ines <imch8383@gmail.com>
2026-03-21 17:02:24 +0100

    Refactor server.js for improved API handling

--- server.js
+++ server.js
@@ -14,64 +14,87 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Serve static files from the client directory
 app.use(express.static(path.join(__dirname, '../client')));
 
-// Serve index.html for any other route (fix for PathError on Render)
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
 
 
-// CRUD API
+//create post api 
 app.post("/notes", (req, res) => {
     const body = req.body;
-    db.addNote(body)
-      .then(data => res.send(data))
-      .catch(err => res.status(500).send(err));
+    console.log("body:", body);
+    db.addNote(body).then(data => {
+        res.send(data);
+    }).catch((err) => {
+        res.status(500);
+    });
+
 });
 
+
 app.get("/notes", (req, res) => {
-    const { title } = req.query;
-    if (title) {
-        db.getNoteByTitle(title)
-          .then(data => res.send(data))
-          .catch(err => res.status(500).send(err));
-    } else {
-        db.getNote()
-          .then(data => res.send(data))
-          .catch(err => res.status(500).send(err));
+    const {title}=req.query;
+    if (title){
+        db.getNoteByTitle(title).then(data => {
+        res.send(data);
+    }).catch((err) => {
+        res.status(500).send(err);
+    }) 
+    }else{
+         db.getNote().then(data => {
+        res.send(data);
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
     }
-});
+   
+})
 
 app.get('/notes/:id', (req, res) => {
     const { id } = req.params;
-    db.getNoteByID(id)
-      .then(data => {
-          if (!data) res.status(404).send("Note id does not exist: " + id);
-          else res.send(data);
-      })
-      .catch(err => res.status(500).send(err));
-});
+    db.getNoteByID(id).then(data => {
+        if (!data) {
+            res.status(404).send("Note id dos not exist  "+id);
+
+        }
+        else {
+            res.send(data);
+        }
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
+})
+
 
 app.put('/notes', (req, res) => {
-    db.UpdateNote(req.body)
-      .then(data => {
-          if (!data) res.status(404).send("Note id does not exist");
-          else res.send(data);
-      })
-      .catch(err => res.status(500).send(err));
-});
+   
+    db.UpdateNote(req.body).then(data => {
+        if (!data) {
+            res.status(404).send("Note id dos not exist  " +id);
+
+        }
+        else {
+            res.send(data);
+        }
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
+})
 
 app.delete('/notes/:id', (req, res) => {
     const { id } = req.params;
-    db.deleteNote(id)
-      .then(data => {
-          if (!data) res.status(404).send("Note id does not exist: " + id);
-          else res.send(data);
-      })
-      .catch(err => res.status(500).send(err));
-});
+    db.deleteNote(id).then(data => {
+        if (!data) {
+            res.status(404).send("Note id dos not exist  "+id);
 
-const port = process.env.PORT || 3000;
+        }
+        else {
+            res.send(data);
+        }
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
+})
+
+port = process.env.PORT|| 3000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    db.connect();
+    console.log(`server is running on port ${port}`);
+    db.connect()
 });
