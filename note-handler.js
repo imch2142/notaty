9a730069addc56603199a8449c29f044aed967e3
Mecheter Ines <imch8383@gmail.com>
2026-03-21 17:25:48 +0100

    Update note-handler.js

--- note-handler.js
+++ note-handler.js
@@ -1,41 +1,37 @@
-function updateNotesTable(noteId,noteTitle){
-    var table=document.getElementById("notes-table");
-    var rowCount=table.rows.length;
-    while(--rowCount){
-        table.deleteRow(rowCount);
-    }
+function updateNotesTable(noteId, noteTitle) {
+    const tbody = document.getElementById("notes-table").querySelector("tbody");
+    tbody.innerHTML = ""; // يفرغ الجدول
 
-    
-
-    getNote(noteTitle).then(data=>{
+    getNote(noteTitle).then(data => {
         data.forEach(note => {
-            var row=table.insertRow(1);
-            var idArtt=document.createAttribute("id");
-            idArtt.value=note['_id'];
-            row.setAttributeNode (idArtt);
+            const row = tbody.insertRow();
+            row.id = note['_id'];
 
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+            const cell4 = row.insertCell(3);
 
-            var cell1=row.insertCell(0);
-            var cell2=row.insertCell(1);
-            var cell3=row.insertCell(2);
-            var cell4=row.insertCell(3);
-            cell1.innerHTML=note["title"];
-            cell2.innerHTML=note["content"];
-            cell3.innerHTML=note["updatedDate"];
-            cell4.innerHTML=`<a onclick="openEditModel('${note["_id"]}')" href="#"><img src="images/edit.png" style="width:22px;"><a>
-                             <a onclick="confirmDeleteNote('${note["_id"]}')" href="#"><img src="images/delete.png" style="width:22px;"><a>`
-
-
+            cell1.innerText = note.title;
+            cell2.innerText = note.content;
+            cell3.innerText = note.updatedDate;
+            cell4.innerHTML = `
+                <a onclick="openEditModel('${note._id}')" href="#">
+                    <img src="images/edit.png" style="width:22px;">
+                </a>
+                <a onclick="confirmDeleteNote('${note._id}')" href="#">
+                    <img src="images/delete.png" style="width:22px;">
+                </a>
+            `;
         });
-    }).then(()=>{
-        if(noteId){
-            var row=document.getElementById(noteId);
-            row.setAttribute("style","animation:new-row 5s;");
+    }).then(() => {
+        if (noteId) {
+            const row = document.getElementById(noteId);
+            if (row) row.style.animation = "new-row 5s";
         }
-    })
-
-
+    }).catch(err => console.error(err));
 }
+
 
 function searchNotes(){
     const searchTitle=document.getElementById("searchInput").value;
